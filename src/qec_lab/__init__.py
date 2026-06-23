546a68ecef8000cc71bea68cbe31e8482540fdde
GandalfHello145 <161626116+GandalfHello145@users.noreply.github.com>
2026-06-23 11:19:49 +0200

    Export optional PyMatching adapter API

--- src/qec_lab/__init__.py
+++ src/qec_lab/__init__.py
@@ -9,6 +9,11 @@ from qec_lab.matching import log_odds_weight, matrix_syndrome
 from qec_lab.matching import repetition_parity_check_matrix
 from qec_lab.phenomenological import PhenomenologicalRepetitionExperiment
 from qec_lab.phenomenological import PhenomenologicalSample
+from qec_lab.pymatching_adapter import PyMatchingModel, PyMatchingNotInstalledError
+from qec_lab.pymatching_adapter import build_pymatching_model
+from qec_lab.pymatching_adapter import decode_phenomenological_detection_events
+from qec_lab.pymatching_adapter import decode_phenomenological_sample
+from qec_lab.pymatching_adapter import decode_repetition_syndrome
 from qec_lab.repetition import RepetitionCode, SimulationResult
 
 __all__ = [
@@ -18,12 +23,18 @@ __all__ = [
     "MatchingGraph",
     "PhenomenologicalRepetitionExperiment",
     "PhenomenologicalSample",
+    "PyMatchingModel",
+    "PyMatchingNotInstalledError",
     "RepetitionCode",
     "SimulationResult",
     "SingleQubitError",
     "bit_flip_repetition_css_code",
     "build_phenomenological_repetition_matching_graph",
+    "build_pymatching_model",
     "build_repetition_matching_graph",
+    "decode_phenomenological_detection_events",
+    "decode_phenomenological_sample",
+    "decode_repetition_syndrome",
     "log_odds_weight",
     "matrix_syndrome",
     "repetition_parity_check_matrix",
