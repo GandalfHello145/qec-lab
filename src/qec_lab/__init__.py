7ce5c5a36c2091c3d7b1bbda3719a71ab05addcc
GandalfHello145 <161626116+GandalfHello145@users.noreply.github.com>
2026-06-23 11:27:19 +0200

    Export PyMatching phenomenological estimator

--- src/qec_lab/__init__.py
+++ src/qec_lab/__init__.py
@@ -10,10 +10,14 @@ from qec_lab.matching import repetition_parity_check_matrix
 from qec_lab.phenomenological import PhenomenologicalRepetitionExperiment
 from qec_lab.phenomenological import PhenomenologicalSample
 from qec_lab.pymatching_adapter import PyMatchingModel, PyMatchingNotInstalledError
+from qec_lab.pymatching_adapter import PyMatchingPhenomenologicalResult
 from qec_lab.pymatching_adapter import build_pymatching_model
+from qec_lab.pymatching_adapter import correct_final_state_with_decoded_faults
 from qec_lab.pymatching_adapter import decode_phenomenological_detection_events
 from qec_lab.pymatching_adapter import decode_phenomenological_sample
 from qec_lab.pymatching_adapter import decode_repetition_syndrome
+from qec_lab.pymatching_adapter import estimate_phenomenological_logical_error_rate_with_pymatching
+from qec_lab.pymatching_adapter import run_phenomenological_trial_with_pymatching
 from qec_lab.repetition import RepetitionCode, SimulationResult
 
 __all__ = [
@@ -25,6 +29,7 @@ __all__ = [
     "PhenomenologicalSample",
     "PyMatchingModel",
     "PyMatchingNotInstalledError",
+    "PyMatchingPhenomenologicalResult",
     "RepetitionCode",
     "SimulationResult",
     "SingleQubitError",
@@ -32,12 +37,15 @@ __all__ = [
     "build_phenomenological_repetition_matching_graph",
     "build_pymatching_model",
     "build_repetition_matching_graph",
+    "correct_final_state_with_decoded_faults",
     "decode_phenomenological_detection_events",
     "decode_phenomenological_sample",
     "decode_repetition_syndrome",
+    "estimate_phenomenological_logical_error_rate_with_pymatching",
     "log_odds_weight",
     "matrix_syndrome",
     "repetition_parity_check_matrix",
+    "run_phenomenological_trial_with_pymatching",
     "shor_code",
     "steane_code",
 ]
